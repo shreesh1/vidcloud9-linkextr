1ed792f6166d6704394982f44b15b799f00ce1ba
Shreesh Roliwal <sroliwal@gmail.com>
2021-04-27 19:45:21 +0530

    XStreamCDN support added

--- background.js
+++ background.js
@@ -1,4 +1,9 @@
 function listener(details) {
+  if(details.url.match(/vidcloud9/g))
+  {
+    var h = details.url.toString();
+    console.log(h.slice(29,h.length));
+  }
   if(details.url.match(/dood/g))
   {
     console.log(details.url);
@@ -7,7 +12,7 @@ function listener(details) {
   {
     console.log(details.url);
   }
-  else
+  else if(details.url.match(/vidnext/g))
   {
 
   
@@ -40,11 +45,29 @@ function listener(details) {
     filter.disconnect();
   }
 }
+else if(details.url.match(/fcdn/g))
+{
+  let filter = browser.webRequest.filterResponseData(details.requestId);
+  let decoder = new TextDecoder("utf-8");
+  let encoder = new TextEncoder();
+
+  filter.ondata = event => {
+    let str = decoder.decode(event.data, {stream: true});
+    var k = str.toString();
+    var p = JSON.parse(k);
+    console.log("For 480p " + p.data[0].file);
+    console.log("For 720p " + p.data[1].file);
+    console.log("For 1080p " + p.data[2].file);
+    filter.write(encoder.encode(str));
+    filter.disconnect();
+  }
+}
   return {};
+
 }
 
 browser.webRequest.onBeforeRequest.addListener(
   listener,
-  {urls: ["https://vidnext.net/ajax.php?*","https://dood.to/e/*","https://storage.googleapis.com/*"]},
+  {urls: ["https://vidnext.net/ajax.php?*","https://dood.to/e/*","https://storage.googleapis.com/*","https://fcdn.stream/api/source/*","https://vidcloud9.com/videos/*"]},
   ["blocking"]
 );
